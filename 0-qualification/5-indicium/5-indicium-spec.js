d6059d206beb1fb7d289733a67600d8e0ce98e7c
gergooo <39014407+gergooo@users.noreply.github.com>
2020-04-05 02:17:39 +0200

    0/5 Indicium - not quite good

--- 0-qualification/5-indicium/5-indicium-spec.js
+++ 0-qualification/5-indicium/5-indicium-spec.js
@@ -2,21 +2,225 @@
 
 global.test = true;
 
-const {lineReaderCallback} = require('./5-indicium');
+const {lineReaderCallback, buildDiagonal} = require('./5-indicium');
 
-describe.only('Nesting depth', function() {
-  it('example test', function() {
+describe.only('Indicium', function() {
+  it.skip('example test', function() {
     testForInputAndOutput(
         `2
         3 6
         2 3`,
 
         `Case #1: POSSIBLE
-        2 1 3
-        3 2 1
-        1 3 2
+        3 1 2
+        1 2 3
+        2 3 1
         Case #2: IMPOSSIBLE`,
     );
+  });
+
+  context('simple ones', function() {
+    it('2x2', function() {
+      testForInputAndOutput(
+          `1
+           2 2`,
+
+          `Case #1: POSSIBLE
+        1 2
+        2 1`,
+      );
+    });
+    it('5x5', function() {
+      testForInputAndOutput(
+          `1
+           5 5`,
+
+          `Case #1: POSSIBLE
+        1 2 3 4 5
+        5 1 2 3 4
+        4 5 1 2 3
+        3 4 5 1 2
+        2 3 4 5 1`,
+      );
+    });
+  });
+
+  context('not that simple ones, but still simple', function() {
+    it('2x2', function() {
+      testForInputAndOutput(
+          `1
+        2 4`,
+
+          `Case #1: POSSIBLE
+        2 1
+        1 2`,
+      );
+    });
+    it('3x3', function() {
+      testForInputAndOutput(
+          `3
+        3 3
+        3 6
+        3 9`,
+
+          `Case #1: POSSIBLE
+        1 2 3
+        3 1 2
+        2 3 1
+        Case #2: POSSIBLE
+        3 1 2
+        1 2 3
+        2 3 1
+        Case #3: POSSIBLE
+        3 1 2
+        2 3 1
+        1 2 3`,
+      );
+    });
+    it.only('4x4', function() {
+      testForInputAndOutput(
+          `4
+        4 4
+        4 6
+        4 7
+        4 8`,
+
+          `Case #1: POSSIBLE
+        1 2 3 4
+        4 1 2 3
+        3 4 1 2
+        2 3 4 1
+        Case #2: POSSIBLE
+        2 1 3 4
+        1 2 4 3
+        4 3 1 2
+        3 4 2 1
+        Case #3: POSSIBLE
+        3 1 2 4
+        1 2 4 3
+        4 3 1 2
+        2 4 3 1`,
+      );
+    });
+  });
+
+
+  it('Calculator', function() {
+    const input = 'aabc';
+
+    console.log(
+        input
+            .split('')
+            .map((x) => x.charCodeAt(0) - 'a'.charCodeAt(0) + 1)
+            .reduce((sum, x) => sum + x));
+  });
+
+  context('impossible cases', function() {
+    it('2', function() {
+      testForInputAndOutput(
+          `3
+           2 1
+           2 3
+           2 5`,
+
+          `Case #1: IMPOSSIBLE
+           Case #2: IMPOSSIBLE
+           Case #3: IMPOSSIBLE`,
+      );
+    });
+
+    it('3', function() {
+      testForInputAndOutput(
+          `6
+           3 2
+           3 4
+           3 5
+           3 7
+           3 8
+           3 10`,
+
+          `Case #1: IMPOSSIBLE
+           Case #2: IMPOSSIBLE
+           Case #3: IMPOSSIBLE
+           Case #4: IMPOSSIBLE
+           Case #5: IMPOSSIBLE
+           Case #6: IMPOSSIBLE`,
+      );
+    });
+
+    it('4', function() {
+      testForInputAndOutput(
+          `4
+           4 3
+           4 5
+           4 15
+           4 17`,
+
+          `Case #1: IMPOSSIBLE
+           Case #2: IMPOSSIBLE
+           Case #3: IMPOSSIBLE
+           Case #4: IMPOSSIBLE`,
+      );
+    });
+  });
+
+  context('Diagonal building', function() {
+    it('4x4 diagonal building 16', function() {
+      buildDiagonal(4, 16).join('').should.deep.equal('4444');
+    });
+    it('4x4 diagonal building 14', function() {
+      buildDiagonal(4, 14).join('').should.deep.equal('4433');
+    });
+    it('4x4 diagonal building 13', function() {
+      buildDiagonal(4, 13).join('').should.deep.equal('4432');
+    });
+    it('4x4 diagonal building 12', function() {
+      buildDiagonal(4, 12).join('').should.deep.equal('4431');
+    });
+    it('4x4 diagonal building 11', function() {
+      buildDiagonal(4, 11).join('').should.deep.equal('4421');
+    });
+    it('4x4 diagonal building 10', function() {
+      buildDiagonal(4, 10).join('').should.deep.equal('4411');
+    });
+    it('4x4 diagonal building 9', function() {
+      buildDiagonal(4, 9).join('').should.deep.equal('4311');
+    });
+    it('4x4 diagonal building 8', function() {
+      buildDiagonal(4, 8).join('').should.deep.equal('4211');
+    });
+    it('4x4 diagonal building 7', function() {
+      buildDiagonal(4, 7).join('').should.deep.equal('3211');
+    });
+    it('4x4 diagonal building 6', function() {
+      buildDiagonal(4, 6).join('').should.deep.equal('2211');
+    });
+    it('4x4 diagonal building 4', function() {
+      buildDiagonal(4, 4).join('').should.deep.equal('1111');
+    });
+    it('3x3 diagonal building', function() {
+      buildDiagonal(3, 3).join('').should.deep.equal('111');
+      buildDiagonal(3, 6).join('').should.deep.equal('321');
+      buildDiagonal(3, 9).join('').should.deep.equal('333');
+    });
+    it('2x2 diagonal building', function() {
+      buildDiagonal(2, 2).join('').should.deep.equal('11');
+      buildDiagonal(2, 4).join('').should.deep.equal('22');
+    });
+    it('5x5 diagonal building', function() {
+      buildDiagonal(5, 5).join('').should.deep.equal('11111');
+      buildDiagonal(5, 7).join('').should.deep.equal('22111');
+      buildDiagonal(5, 8).join('').should.deep.equal('32111');
+      buildDiagonal(5, 13).join('').should.deep.equal('55111');
+      buildDiagonal(5, 18).join('').should.deep.equal('55521');
+      buildDiagonal(5, 23).join('').should.deep.equal('55544');
+      buildDiagonal(5, 25).join('').should.deep.equal('55555');
+    });
+    it('50x50 diagonal building', function() {
+      buildDiagonal(50, 2500).should.deep.equal([50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50]);
+      buildDiagonal(50, 2498).should.deep.equal([50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 49, 49]);
+      buildDiagonal(50, 1634).should.deep.equal([50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 17, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]);
+    });
   });
 
   function testForInputAndOutput(input, expectedOutput) {
@@ -24,7 +228,7 @@ describe.only('Nesting depth', function() {
 
     const callbackUnderTest = lineReaderCallback(
         {close: () => {}},
-        (result) => actualOutputLines.push(result),
+        (result) => actualOutputLines.push(...result.split('\n')),
     );
 
     const inputLines = input.split('\n').map((x) =>x.trim());
--- 0-qualification/5-indicium/5-indicium.js
+++ 0-qualification/5-indicium/5-indicium.js
@@ -47,12 +47,126 @@ function returnWithResults(input, outputCallback) {
 
 
 function solveTestCase(inputString) {
-  return 'IMPOSSIBLE';
+  const [N, K] = inputString.split(' ').map((x) => Number(x));
+
+  if (N == 2 && ![2, 4].includes(K)) {
+    return 'IMPOSSIBLE';
+  } else if (N == 3 && ![3, 6, 9].includes(K)) {
+    return 'IMPOSSIBLE';
+  } else if (!(K == N || K == N*N || (K > N+1 && K < N*N-1))) {
+    return 'IMPOSSIBLE';
+  }
+
+  const result = Array(N);
+  const columns = Array(N);
+  const rows = Array(N);
+  for (let i = 0; i < N; i++) {
+    result[i] = Array(N);
+    columns[i] = {};
+    rows[i] = {};
+  }
+
+  const diagonal = buildDiagonal(N, K);
+  diagonal.forEach((elem, i) => {
+    result[i][i] = elem;
+    columns[i][elem] = 1;
+    rows[i][elem] = 1;
+  });
+
+  const path = [];
+  const removedPath = [];
+  let removedCandidates = {};
+  let watchdog = 15;
+
+  for (let x = 0; x < N; x++) {
+    for (let y = 0; y < N; y++) {
+      if (x != y) {
+        let candidate = getCandidate(y, x, result, N);
+
+        let counter = 0;
+        while ((columns[y][candidate] || rows[x][candidate] || removedCandidates[candidate]) && counter < N) {
+          candidate++;
+          candidate = (candidate > N) ? 1 : candidate;
+          counter++;
+        }
+
+        if (counter != N) {
+          removedCandidates = {};
+          result[x][y] = candidate;
+          columns[y][candidate] = 1;
+          rows[x][candidate] = 1;
+          // removedStep = undefined;
+
+          path.push({x, y, candidate});
+        } else {
+          const step = path.pop();
+          result[step.x][step.y] = undefined;
+          columns[step.y][step.candidate] = undefined;
+          rows[step.x][step.candidate] = undefined;
+
+          removedCandidates[step.candidate] = 1;
+
+          x = step.x;
+          y = step.y - 1;
+          watchdog--;
+
+          removedPath.push(step);
+        }
+      }
+
+      if (watchdog == 0) {
+        return 'POSSIBLE\n' + result.map((line) => line.join(' ')).join('\n');
+      }
+    }
+  }
+
+  return 'POSSIBLE\n' + result.map((line) => line.join(' ')).join('\n');
+}
+
+function getCandidate(y, x, result, N) {
+  let candidate;
+  if (y > 0) {
+    if (y == x + 1 && result[x][y - 1] > 1) {
+      candidate = 1;
+    } else {
+      candidate = (result[x][y - 1] + 1);
+      candidate = (candidate > N) ? 1 : candidate;
+    }
+  } else {
+    candidate = result[x - 1][y] - 1;
+    candidate = (candidate == 0) ? N : candidate;
+  }
+  return candidate;
+}
+
+function buildDiagonal(N, K) {
+  const result = Array(N);
+
+  for (let i = 0; i < N; i++) {
+    if (N > (K - (N - i - 1))) {
+      result[i] = (K - (N - i - 1));
+    } else {
+      result[i] = N;
+    }
+
+    K -= result[i];
+
+    if (i == 0 && result[i] > 1 && K == N - 1) {
+      K++;
+      result[i]--;
+    }
+
+    if (i == N - 1 && result[i] < N && result[i-1] == N) {
+      result[i]++;
+      result[i-1]--;
+    }
+  }
+  return result;
 }
 
 
 if (global.test) {
-  module.exports = {lineReaderCallback};
+  module.exports = {lineReaderCallback, buildDiagonal};
 } else {
   main();
 }
