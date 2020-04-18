2fa5bf9e5b9dd4d38b808c1b4474d3c92c4917e2
gergooo <39014407+gergooo@users.noreply.github.com>
2020-04-18 22:54:34 +0200

    0/4 ESAb ATAd - first iteration

---
+++ 0-qualification/4-ESAb-ATAd-interactive/4-ESAb-ATAd-interactive-spec.js
@@ -0,0 +1,142 @@
+'use strict';
+
+global.test = true;
+
+const {lineReaderCallback} = require('./4-ESAb-ATAd-interactive');
+
+describe.only('ESAb ATAd', function() {
+  const noOutput = 'no output';
+  let query = noOutput;
+  let callbackUnderTest;
+  let closeCallCount = 0;
+
+  beforeEach(function() {
+    closeCallCount = 0;
+    query = noOutput;
+  });
+
+
+  it('init and start', function() {
+    callbackUnderTest = initInteractiveProblem('1 10');
+
+    expectThisAndAnswerThat('1', '0');
+    expectThisAndAnswerThat('10', '0');
+  });
+
+  it('iterate through queries', function() {
+    callbackUnderTest = initInteractiveProblem('1 10');
+
+    expectThisAndAnswerThat('1', '0');
+    expectThisAndAnswerThat('10', '0');
+    expectThisAndAnswerThat('2', '0');
+    expectThisAndAnswerThat('9', '1');
+    expectThisAndAnswerThat('3', '1');
+    expectThisAndAnswerThat('8', '0');
+    expectThisAndAnswerThat('4', '1');
+    expectThisAndAnswerThat('7', '0');
+    expectThisAndAnswerThat('5', '1');
+    expectThisAndAnswerThat('6', '0');
+  });
+
+  it('having a result for B=10', function() {
+    callbackUnderTest = initInteractiveProblem('1 10');
+
+    expectThisAndAnswerThat('1', '0');
+    expectThisAndAnswerThat('10', '0');
+    expectThisAndAnswerThat('2', '0');
+    expectThisAndAnswerThat('9', '1');
+    expectThisAndAnswerThat('3', '1');
+    expectThisAndAnswerThat('8', '0');
+    expectThisAndAnswerThat('4', '1');
+    expectThisAndAnswerThat('7', '0');
+    expectThisAndAnswerThat('5', '1');
+    expectThisAndAnswerThat('6', '0');
+    expectThisAndAnswerThat('0011100010', 'Y');
+
+    closeCallCount.should.equal(1);
+  });
+
+  it('having a result for B=10, two testcases', function() {
+    callbackUnderTest = initInteractiveProblem('2 10');
+
+    expectThisAndAnswerThat('1', '0');
+    expectThisAndAnswerThat('10', '0');
+    expectThisAndAnswerThat('2', '0');
+    expectThisAndAnswerThat('9', '1');
+    expectThisAndAnswerThat('3', '1');
+    expectThisAndAnswerThat('8', '0');
+    expectThisAndAnswerThat('4', '1');
+    expectThisAndAnswerThat('7', '0');
+    expectThisAndAnswerThat('5', '1');
+    expectThisAndAnswerThat('6', '0');
+    expectThisAndAnswerThat('0011100010', 'Y');
+
+    expectThisAndAnswerThat('1', '1');
+    expectThisAndAnswerThat('10', '0');
+    expectThisAndAnswerThat('2', '1');
+    expectThisAndAnswerThat('9', '1');
+    expectThisAndAnswerThat('3', '1');
+    expectThisAndAnswerThat('8', '0');
+    expectThisAndAnswerThat('4', '1');
+    expectThisAndAnswerThat('7', '0');
+    expectThisAndAnswerThat('5', '1');
+    expectThisAndAnswerThat('6', '0');
+    closeCallCount.should.equal(0);
+    expectThisAndAnswerThat('1111100010', 'Y');
+
+    closeCallCount.should.equal(1);
+    query.should.equal(noOutput);
+  });
+
+  it('stop on wrong answer', function() {
+    callbackUnderTest = initInteractiveProblem('2 10');
+
+    expectThisAndAnswerThat('1', '0');
+    expectThisAndAnswerThat('10', '0');
+    expectThisAndAnswerThat('2', '0');
+    expectThisAndAnswerThat('9', '1');
+    expectThisAndAnswerThat('3', '1');
+    expectThisAndAnswerThat('8', '0');
+    expectThisAndAnswerThat('4', '1');
+    expectThisAndAnswerThat('7', '0');
+    expectThisAndAnswerThat('5', '1');
+    expectThisAndAnswerThat('6', '0');
+    expectThisAndAnswerThat('0011100010', 'N');
+
+    query.should.equal(noOutput);
+    closeCallCount.should.equal(1);
+  });
+
+  it('stop on "N" any time', function() {
+    callbackUnderTest = initInteractiveProblem('2 10');
+
+    expectThisAndAnswerThat('1', '0');
+    expectThisAndAnswerThat('10', '0');
+    expectThisAndAnswerThat('2', '0');
+    expectThisAndAnswerThat('9', '1');
+    expectThisAndAnswerThat('3', '1');
+    expectThisAndAnswerThat('8', 'N');
+
+    query.should.equal(noOutput);
+    closeCallCount.should.equal(1);
+  });
+
+  function initInteractiveProblem(input) {
+    const callbackUnderTest = lineReaderCallback(
+        {close: () => closeCallCount++},
+        (result) => query = result.toString());
+
+    callbackUnderTest(input);
+
+    return callbackUnderTest;
+  }
+
+  function expectThisAndAnswerThat(expectation, answer) {
+    query.should.equal(expectation);
+    query = noOutput;
+
+    callbackUnderTest(answer);
+  }
+});
+
+
---
+++ 0-qualification/4-ESAb-ATAd-interactive/4-ESAb-ATAd-interactive.js
@@ -0,0 +1,57 @@
+'use strict';
+
+function main() {
+  const readline = require('readline');
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  rl.on('line', lineReaderCallback(rl, console.log));
+}
+
+function lineReaderCallback(rl, outputCallback) {
+  const state = {
+    T: undefined,
+    Ti: 1,
+    B: undefined,
+    x: undefined,
+    array: [],
+  };
+
+  return (line) => {
+    if (!state.T) {
+      ([state.T, state.B] = line.split(' ').map((x) => Number(x)));
+      state.x = 1;
+    } else {
+      if (line == 'Y') {
+        // next problem or finish
+        state.array = [];
+        state.x = 1;
+        state.Ti++;
+      } else if (state.array[state.x - 1] == undefined) {
+        state.array[state.x - 1] = Number(line);
+      } else {
+        state.array[state.B - state.x] = Number(line);
+        state.x++;
+      }
+    }
+
+    if (line == 'N' || state.Ti > state.T) {
+      rl.close();
+    } else if (state.x > state.B/2) {
+      outputCallback(state.array.map((x) => x.toString()).join(''));
+    } else if (state.array[state.x - 1] == undefined) {
+      outputCallback(state.x);
+    } else {
+      outputCallback(state.B - state.x + 1);
+    }
+  };
+}
+
+
+if (global.test) {
+  module.exports = {lineReaderCallback};
+} else {
+  main();
+}
