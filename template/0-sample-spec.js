4d00175a1b21c389f41cb0ea75bb0a9f89cf6f40
gergooo <39014407+gergooo@users.noreply.github.com>
2020-04-19 14:16:45 +0200

    Old templates removed

--- template/0-sample-spec.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const {calculateSolution} = require('./0-sample');
-
-describe('Sample', function() {
-  it('first test', function() {
-    calculateSolution(2, 3).should.equal(5);
-  });
-});
--- template/0-sample.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-function calculateSolution(a, b) {
-  return a + b;
-}
-
-module.exports = {calculateSolution};
