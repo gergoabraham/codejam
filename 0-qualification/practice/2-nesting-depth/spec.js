008a4d1554b0eb21d976e63fcf397b93dcfddb37
gergooo <39014407+gergooo@users.noreply.github.com>
2020-04-28 14:39:01 +0200

    0-2 Nesting depth - starting

--- 0-qualification/practice/2-nesting-depth/spec.js
+++ 0-qualification/practice/2-nesting-depth/spec.js
@@ -4,30 +4,20 @@ global.testEnvironment = true;
 
 const {getLineReaderCallback} = require('./solution');
 
-describe('Vestigium', function() {
+describe.only('Nesting depth', function() {
   it('example test', function() {
     testForInputAndOutput(
-        `3
-        4
-        1 2 3 4
-        2 1 4 3
-        3 4 1 2
-        4 3 2 1
-        4
-        2 2 2 2
-        2 3 2 3
-        2 2 2 3
-        2 2 2 2
-        3
-        2 1 3
-        1 3 2
-        1 2 3`,
+        `2
+        1 2 3
+        4 5 6`,
 
-        `Case #1: 4 0 0
-        Case #2: 9 4 4
-        Case #3: 8 0 2`,
+        `Case #1: POSSIBLE
+        1 2 3
+        Case #2: POSSIBLE
+        4 5 6`,
     );
   });
+
 
   /** Testing helpers from here ***********************************************/
   function testForInputAndOutput(input, expectedOutput) {
